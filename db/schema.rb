f9f2ee3c5e5afa0a789399fc32f52bb4d36bfed1
tanum-3367 <uong.minh.tan@sun-asterisk.com>
2024-07-05 14:06:15 +0900

    feature: setup order to database and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_05_020622) do
+  create_table "orders", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
